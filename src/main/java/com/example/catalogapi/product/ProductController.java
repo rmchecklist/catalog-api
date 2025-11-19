4582be1d64f10df76116c53dd702c19f737a22b9
Rengith Manickam <mrengith@gmail.com>
2025-11-19 07:07:35 -0500

    fixed supabase credentials

--- src/main/java/com/example/catalogapi/product/ProductController.java
+++ src/main/java/com/example/catalogapi/product/ProductController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("api/products")
 public class ProductController {
 
     private final ProductService productService;
